ade3bd6294c55970253f7be998504307b571ec73
Halfdan Rydbeck <hrydbeck@gmail.com>
2025-11-05 10:14:07 +0100

    chore(reorg): remove root-level wrapper files; canonical files live under bin/, R/, and scripts/

--- jasen_out_watcher.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-// Lightweight wrapper to keep the old path working while the real entrypoint lives in bin/
-require('./bin/jasen_out_watcher.js');
--- run_reportree.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-// Wrapper that delegates to bin/run_reportree.js so the entrypoint lives in bin/
-require('./bin/run_reportree.js');
